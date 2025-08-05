976af0f0a603eb06fc8831f2c8884c49b9732cdf
Kelly Cobean <kelly.cobean@sap.com>
2025-08-05 11:56:50 -0500

    rename to main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "awslab" {
-  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  ami           = aws_ami.amzn-linux-2023-ami.id
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.aws_lab_subnet.id
+
+  tags = {
+    Name = "tf-example"
+  }
 }
