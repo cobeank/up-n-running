7d4a198089fbd3e116445cf62a5c1f74d1296e78
Kelly Cobean <kelly.cobean@sap.com>
2025-08-19 21:30:36 -0500

    fixed EOF

--- main.tf
+++ main.tf
@@ -114,21 +114,22 @@ resource "aws_instance" "awslab" {
 
   user_data = <<-EOF
     #!/bin/bash -x
-    apt update
-    echo "<VirtualHost *:8080>" | tee /etc/apache2/sites-available/000-default.conf
-    echo "ServerAdmin webmaster@localhost" | tee -a /etc/apache2/sites-available/000-default.conf
-    echo "DocumentRoot /var/www/html" | tee -a /etc/apache2/sites-available/000-default.conf 
-    echo "</VirtualHost>" | tee -a /etc/apache2/sites-available/000-default.conf
-    echo "Listen 8080" | tee /etc/apache2/ports.conf
-    echo "<IfModule ssl_module>" | tee -a /etc/apache2/ports.conf
-    echo "Listen 443" | tee -a /etc/apache2/ports.conf
-    echo "</IfModule>" | tee -a /etc/apache2/ports.conf
-    echo "<IfModule mod_gnutls.c>" | tee -a /etc/apache2/ports.conf
-    echo " Listen 443" | tee -a /etc/apache2/ports.conf
-    echo "</IfModule>" | tee -a /etc/apache2/ports.conf
-    echo "Hello, World, It's Kelly" | tee /var/www/html/index.html
-    echo "And this is Terraform" | tee -a /var/www/html/index.html
-    systemctl restart apache2
+    sudo apt update
+    sudo apt install apache2
+    sudo echo "<VirtualHost *:8080>" | tee /etc/apache2/sites-available/000-default.conf
+    sudo echo "ServerAdmin webmaster@localhost" | tee -a /etc/apache2/sites-available/000-default.conf
+    sudo echo "DocumentRoot /var/www/html" | tee -a /etc/apache2/sites-available/000-default.conf 
+    sudo echo "</VirtualHost>" | tee -a /etc/apache2/sites-available/000-default.conf
+    sudo echo "Listen 8080" | tee /etc/apache2/ports.conf
+    sudo echo "<IfModule ssl_module>" | tee -a /etc/apache2/ports.conf
+    sudo echo "Listen 443" | tee -a /etc/apache2/ports.conf
+    sudo echo "</IfModule>" | tee -a /etc/apache2/ports.conf
+    sudo echo "<IfModule mod_gnutls.c>" | tee -a /etc/apache2/ports.conf
+    sudo echo " Listen 443" | tee -a /etc/apache2/ports.conf
+    sudo echo "</IfModule>" | tee -a /etc/apache2/ports.conf
+    sudo echo "Hello, World, It's Kelly" | tee /var/www/html/index.html
+    sudo echo "And this is Terraform" | tee -a /var/www/html/index.html
+    sudo systemctl restart apache2
     
     EOF
 
