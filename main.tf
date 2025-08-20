cfe888735b2cbe91a07f38178c3c9b1f3a3f6d88
Kelly Cobean <kelly.cobean@sap.com>
2025-08-19 19:46:07 -0500

    catch up

--- main.tf
+++ main.tf
@@ -1,8 +1,106 @@
-resource "aws_instance" "awslab" {
-  ami           = "ami-0ca5a2f40c2601df6"
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "web_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "web_server_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "inet_gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "internet-gw"
+  }
+}
+resource "aws_route_table" "vpc_main_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.inet_gw.id
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.web_subnet.id
+  route_table_id = aws_route_table.vpc_main_route_table.id
+}
+resource "aws_security_group" "web_access" {
+  name        = "web-ingress-sg"
+  description = "Allow inbound web traffic to TCP/8080"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "Allow inbound web traffic"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ingress_icmp" {
+  security_group_id = aws_security_group.web_access.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_ingress_web_traffic" {
+  security_group_id = aws_security_group.web_access.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ingress_ssh" {
+  security_group_id = aws_security_group.web_access.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_out" {
+  security_group_id = aws_security_group.web_access.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = "-1"
+  to_port           = "-1"
+  ip_protocol       = "-1"
+}
+
+resource "aws_instance" "awslab" {
+  ami                         = "ami-001e387a55c961ff2"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.web_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_access.id]
+  associate_public_ip_address = true
+  key_name                    = "cobeank_pub_key"
   tags = {
     Name = "web-server1"
   }
+
+  depends_on = [aws_internet_gateway.inet_gw]
+
+
+  user_data = <<-EOF
+    #!/bin/bash -x
+    apt update
+    apt install apache2
+    echo "<VirtualHost *:8080>\nServerAdmin webmaster@localhost\nDocumentRoot /var/www/html\n</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
+    echo "Listen 8080\<IfModule ssl_module>\Listen 443\</IfModule>\<IfModule mod_gnutls.c>\Listen 443\</IfModule> > /etc/apache2/ports.conf
+    echo "Hello, World, It's Kelly" > /var/www/html/index.html
+    systemctl restart apache2
+    EOF
+
+  user_data_replace_on_change = true
 }
